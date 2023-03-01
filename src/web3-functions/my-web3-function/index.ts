3ba405e0bcb5948c4fec61d7992b2dcaeb65a527
Josh <55187183+JoshDAO@users.noreply.github.com>
2023-03-01 17:54:14 +1100

    edit return payload

--- src/web3-functions/my-web3-function/index.ts
+++ src/web3-functions/my-web3-function/index.ts
@@ -1,8 +1,5 @@
-import {
-  Web3Function,
-  Web3FunctionContext,
-} from "@gelatonetwork/web3-functions-sdk";
-import { Contract , Event} from "ethers";
+import { Web3Function, Web3FunctionContext } from "@gelatonetwork/web3-functions-sdk"
+import { Contract, Event } from "ethers"
 
 import controllerAbi from "../../abi/NewController.json"
 import optionRegistryAbi from "../../abi/OptionRegistry.json"
@@ -11,30 +8,27 @@ import vaultCollateralMulticallAbi from "../../abi/VaultCollateralMulticall.json
 // block that the option regsitry was deployed on
 const optionRegistryDeployBlock = 25976032
 
-
 // Fill this out with your Web3 Function logic
 Web3Function.onRun(async (context: Web3FunctionContext) => {
-  const { storage, provider } = context;
-  
-  const optionRegistryAddress = "0x04706DE6cE851a284b569EBaE2e258225D952368"
-	const controllerAddress = "0x594bD4eC29F7900AE29549c140Ac53b5240d4019"
-  const multicallAddress = "0xa304d4418E6f4dFCEc90A73E478FDB950Fd70385"
+	const { storage, provider } = context
 
-  // the block number on which this function was last called
+	const optionRegistryAddress = "0x04706DE6cE851a284b569EBaE2e258225D952368"
+	const controllerAddress = "0x594bD4eC29F7900AE29549c140Ac53b5240d4019"
+	const multicallAddress = "0xC6b51303B65156bB1C35d188570eb671B7b0Bc50"
+
+	// the block number on which this function was last called
 	let lastQueryBlock
 	// an array of vaultIDs which need their health factor checking
 	let activeVaultIds
 	// the vaultCount for the option registry on the last function call
 	let previousVaultCount
 
-  try {
+	try {
 		// get persistant variables from store
-		lastQueryBlock = parseInt(
-			await storage.get("lastQueryBlock") ?? "0"
-		)
+		lastQueryBlock = parseInt((await storage.get("lastQueryBlock")) ?? "0")
 
-		activeVaultIds = JSON.parse(await storage.get("activeVaultIds") ?? "[]")
-		previousVaultCount = parseInt(await storage.get("previousVaultCount") ?? "0")
+		activeVaultIds = JSON.parse((await storage.get("activeVaultIds")) ?? "[]")
+		previousVaultCount = parseInt((await storage.get("previousVaultCount")) ?? "0")
 		console.log({ lastQueryBlock, activeVaultIds, previousVaultCount })
 	} catch (err) {
 		console.log("error retrieving data from store")
@@ -47,31 +41,23 @@ Web3Function.onRun(async (context: Web3FunctionContext) => {
 		previousVaultCount = 0
 	}
 
-  const optionRegistry = await new Contract(optionRegistryAddress, optionRegistryAbi, provider)
-  const controller = await new Contract(controllerAddress, controllerAbi, provider)
-  const multicall = await new Contract(multicallAddress,vaultCollateralMulticallAbi, provider )
+	const optionRegistry = await new Contract(optionRegistryAddress, optionRegistryAbi, provider)
+	const controller = await new Contract(controllerAddress, controllerAbi, provider)
+	const multicall = await new Contract(multicallAddress, vaultCollateralMulticallAbi, provider)
 
 	const currentBlock = await provider.getBlockNumber()
 	// will contain emitted SettledVault events since the previous function execution
 	let settleEvents: Event[] = []
 	let liquidationEvents: Event[] = []
-	
-  settleEvents = await controller.queryFilter(
-    controller.filters.VaultSettled(),
-    lastQueryBlock
-  )
-  liquidationEvents = await controller.queryFilter(
-    controller.filters.VaultLiquidated(),
-    lastQueryBlock
-  )
-  settleEvents = settleEvents
-  liquidationEvents = liquidationEvents
 
-  // set last query block to current block value
-	await storage.set(
-		"lastQueryBlock",
-		currentBlock.toString()
+	settleEvents = await controller.queryFilter(controller.filters.VaultSettled(), lastQueryBlock)
+	liquidationEvents = await controller.queryFilter(
+		controller.filters.VaultLiquidated(),
+		lastQueryBlock
 	)
+	settleEvents = settleEvents
+	liquidationEvents = liquidationEvents
+
 	// return vault IDs of settled vault events where the vault owner is the option registry
 	let settledEventIds: Number[] = []
 	if (settleEvents.length) {
@@ -92,41 +78,44 @@ Web3Function.onRun(async (context: Web3FunctionContext) => {
 	console.log("vault count:", vaultCount)
 
 	// create an array of vault IDs that have been created since last execution
-	const additionalVaultIds = Array.from(Array(vaultCount + 1).keys()).slice(
-		previousVaultCount + 1
-	)
+	const additionalVaultIds = Array.from(Array(vaultCount + 1).keys()).slice(previousVaultCount + 1)
 	console.log({ additionalVaultIds })
-	// update previousVaultCount in storage
-	await storage.set("previousVaultCount", vaultCount.toString())
+
 	// add newly created vault IDs to existing array of active vault IDs
 	activeVaultIds.push(...additionalVaultIds)
 	// remove activeVaultIds which appear in settledEventIds
 	activeVaultIds = activeVaultIds.filter(id => !settledEventIds.includes(id))
-	// update activeVaultIDs in storage
-	await storage.set("activeVaultIds", JSON.stringify(activeVaultIds))
 	console.log({ activeVaultIds })
 
-
-  let vaultsToAdjust: Number[] = []
+	let vaultsToAdjust: Number[] = []
 	// multicall function will iterate over all IDs and check their health
 	if (activeVaultIds.length) {
-    // returns vaultID if vault needs adjusting, 0 if it does not.
-    // filter elements that equal 0 out
-    vaultsToAdjust = (await multicall.checkVaults(activeVaultIds)).map(id => id.toNumber()).filter(id => id != 0)
+		// returns vaultID if vault needs adjusting, 0 if it does not.
+		// filter elements that equal 0 out
+		vaultsToAdjust = (await multicall.checkVaults(activeVaultIds))
+			.map(id => id.toNumber())
+			.filter(id => id != 0)
 	}
-  console.log(vaultsToAdjust)
-  // if true, this will signal to the gelato executor to call the adjust function on the multicall contract
-  // the multicall contract will loop through the array calling optionRegistry.adjustCollateral on each vault ID in the callData payload.
-  if (vaultsToAdjust.length){
-    // Return execution call data
-    return {
-      canExec: true,
-      callData: vaultsToAdjust
-    }
-  } else {
-    return {
-      canExec: false,
-      callData: vaultsToAdjust
-    }
-  }
-});
+	console.log(vaultsToAdjust)
+
+	// update activeVaultIDs in storage
+	await storage.set("activeVaultIds", JSON.stringify(activeVaultIds))
+	// update previousVaultCount in storage
+	await storage.set("previousVaultCount", vaultCount.toString())
+	// set last query block to current block value
+	await storage.set("lastQueryBlock", currentBlock.toString())
+	// if true, this will signal to the gelato executor to call the adjust function on the multicall contract
+	// the multicall contract will loop through the array calling optionRegistry.adjustCollateral on each vault ID in the callData payload.
+	if (vaultsToAdjust.length) {
+		// Return execution call data
+		return {
+			canExec: true,
+			callData: multicall.interface.encodeFunctionData("adjustVaults", [vaultsToAdjust])
+		}
+	} else {
+		return {
+			canExec: false,
+			message: "No vault to adjust"
+		}
+	}
+})
